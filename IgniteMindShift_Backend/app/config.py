43da34bf6b7f140ea83d9ea94a7168160b8ea45c
Snehasre S <snehasre03@gmail.com>
2026-05-18 14:42:33 +0530

    Updated CORS

--- IgniteMindShift_Backend/app/config.py
+++ IgniteMindShift_Backend/app/config.py
@@ -22,7 +22,7 @@ class Settings(BaseSettings):
     JWT_REFRESH_TOKEN_EXPIRE_DAYS: int = 7
 
     # CORS
-    CORS_ORIGINS: List[str] = ["http://localhost:3000", "http://localhost:8082"]
+    CORS_ORIGINS: List[str] = ["http://localhost:3000", "http://localhost:8082" , "http://localhost:8083"]
 
     @field_validator("CORS_ORIGINS", mode="before")
     @classmethod
